414f400a3cbabf00f24b65bab96a0284207a2d80
James Wu <391550489@qq.com>
2019-02-26 16:01:46 +0800

    Remove trash code

--- code/control/OBJ/core_cm3.d
+++
@@ -1,2 +0,0 @@
-..\obj\core_cm3.o: ..\CORE\core_cm3.c
-..\obj\core_cm3.o: D:\software\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- code/control/OBJ/startup_stm32f10x_hd.d
+++
@@ -1,1 +0,0 @@
-..\obj\startup_stm32f10x_hd.o: ..\CORE\startup_stm32f10x_hd.s
